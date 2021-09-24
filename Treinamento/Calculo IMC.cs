6325dacf9baeeef8964b83bb225deb4e79169d16
João Mário <joaomario.s.d@gmail.com>
2021-09-24 18:11:21 -0300

    Calculo IMC

---
+++ Treinamento/Calculo IMC.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace Treinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Programa que calcula o IMC de uma pessoa!");
+            Console.WriteLine();
+
+            Paciente pac = new Paciente();
+
+            Console.Write("Digite o nome do paciente: ");
+            pac.Nome = Console.ReadLine();
+
+            Console.Write("Digite a idade do paciente: ");
+            pac.Idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o peso do paciente: ");
+            pac.Peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a altura do paciente: ");
+            pac.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();            
+
+            double imc = pac.Peso / Math.Pow(pac.Altura, 2);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine(pac);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Peso abaixo do normal!");
+            }
+            else if (imc < 24.9)
+            {
+                Console.WriteLine(pac);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Peso normal!");
+            }
+            else if (imc < 29.9)
+            {
+                Console.WriteLine(pac);
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Sobre peso!");
+            }
+            else if (imc < 39.9)
+            {
+                Console.WriteLine(pac);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Grau obsidade");
+            }
+            else if (imc > 40)
+            {
+                Console.WriteLine(pac);
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Obsidade Grave");
+                
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Fim!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Treinamento/Paciente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Treinamento
+{
+    class Paciente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public Paciente()
+        {
+
+        }
+
+        public Paciente(string nome, int idade, double peso, double altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Peso = peso;
+            Altura = altura;
+        }
+
+        public double Imc()
+        {
+           return Peso / Math.Pow(Altura, 2);
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + " Idade: " + Idade + " Anos " 
+                + " Peso: " + Peso.ToString("F2", CultureInfo.InvariantCulture) + "kg " 
+                + " Altura: " + Altura.ToString("F2", CultureInfo.InvariantCulture) + "cm";
+        }
+    }
+}
